82df8e87b9fe8398d4ed18a6f9ecb561919b9a0f
Seyfeddine Jouini <jouini.seyf.js@gmail.com>
2024-02-18 22:02:04 +0100

    adding imports

--- E_commerce-1/src/main/java/com/entities/Product.java
+++ E_commerce-1/src/main/java/com/entities/Product.java
@@ -1,5 +1,10 @@
 package com.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
 public class Product {
 
